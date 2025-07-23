dfe3492257be5f8f387289e1c884c27d81e95a0d
Nageswara Rao D <nageswararao.dolla@gmail.com>
2025-07-23 07:06:48 +0530

    test

--- module/main.tf
+++ module/main.tf
@@ -2,9 +2,9 @@ resource "aws_instance" "instance" {
 
    ami = var.ami_id
    instance_type = var.instance_type
-  vpc_security_group_ids = var.security_groups
+   vpc_security_group_ids = var.security_groups
 
-    tags = {
+   tags = {
       name = var.name
     }
 }
